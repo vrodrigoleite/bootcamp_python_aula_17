e9081254fe8920e5c5bb314335a7351d27192b91
Victor Almeida <vrodrigoleite@gmail.com>
2025-01-30 12:17:12 -0300

    criando o banco de dados

---
+++ exemplo01.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///meubanco.db', echo=True)
+
+print('Conexão com SQLite estabelecida.')
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__= 'usuarios'
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    idade = Column(Integer)
+
+# Criar tabelas no banco de dados
+Base.metadata.create_all(engine)
